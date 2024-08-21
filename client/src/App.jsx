e6e23dcfb5dbe80bdd3cc5df26e6d68bd5d442ca
Anshulk2004 <anshulk0102@gmail.com>
2024-08-21 18:48:23 +0530

    Solved package issues and file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export default function App() {
+    return (
+        <main className={styles.main}>
+          <h2>Hello</h2>
+        </main>
+    );
+}
