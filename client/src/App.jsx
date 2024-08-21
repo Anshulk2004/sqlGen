63836169095d1f389c777515f1a3ac531c33364d
Anshulk2004 <anshulk0102@gmail.com>
2024-08-21 19:11:38 +0530

    add server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
 import styles from "./styles.module.css";
+import { useState } from "react";
+import sqlServer from "./assets/sql-server.png";
 
 export default function App() {
+    const [userPrompt, setUserPrompt] = useState("");
+    const [sqlQuery, setSqlQuery] = useState("");
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const query = await generateQuery();
+        setSqlQuery(query);
+      };
+
     return (
         <main className={styles.main}>
-          <h2>Hello</h2>
+          <img src={sqlServer} className={styles.icon} alt="SQL server" />
+          <h3>Generate SQL with AI</h3>
+          <form onSubmit={onSubmit}>
+            <input
+            type="text"
+            name="query-description"
+            placeholder="Describe your query"
+            onChange={(e) => setUserPrompt(e.target.value)}
+            />
+            <input type="submit" value="Generate query" />
+            </form>
+          
         </main>
     );
 }
