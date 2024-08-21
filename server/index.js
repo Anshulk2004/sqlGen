ec837c4f0ce844f5a937942cd68dc35c6586efbf
Anshulk2004 <anshulk0102@gmail.com>
2024-08-21 21:49:21 +0530

    Some server side changes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import generate from "./generate.js";
 
 const app = express();
 app.use(express.json()); 
@@ -14,7 +15,20 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
   });
   
-  app.listen(port, () => {
+  
+  
+app.post("/generate", async (req, res) => {
+    
+    const { queryDescription } = req.body
+    try {
+        const sqlQuery = await generate(queryDescription);
+        res.json({ response : sqlQuery });
+    } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
   });
-  
